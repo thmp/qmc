8f71272d172d8e0cabf1937b18dfd8759b99a041
thmp <thomas@pischke.me>
2015-11-06 14:20:59 +0100

    Initial commit

---
+++ code/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+int N;					// walkers
+double *x;				// walker positions
+double delta;			// step size
+
+double frand() {
+	return rand() / double(RAND_MAX);
+}
+
+double norm() {
+	static int iset = 0;
+	static double gset;
+	double fac, rsq, v1, v2;
+	if (iset == 0) {
+		do {
+			v1 = 2.0*frand()-1.0;
+			v2 = 2.0*frand()-1.0;
+			rsq = v1*v1 + v2*v2;
+		} while (rsq >= 1.0 || rsq == 0.0);
+		fac = sqrt(-2.0*log(rsq)/rsq);
+		gset = v1*fac;
+		iset = 1;
+		return v2*fac;
+	} else {
+		iset = 0;
+		return gset;
+	}
+}
+
+void initialize() {
+	srand(time(NULL));
+
+	N = 10;
+
+	x = new double[N];
+	for (int i = 0; i < N; i++) {
+		x[i] = frand() - .5;
+	}
+
+	delta = 1;
+
+}
+
+double trial(double x, double alpha) { 
+	return exp(-2 * alpha * x*x);
+}
+
+double weight(double xTrial, double x, double alpha) {
+	return trial(xTrial, alpha) / trial(x, alpha);
+}
+
+double energy(double x, double alpha) {
+	return alpha + x*x * (0.5 - 2*alpha*alpha);
+}
+
+void step_metropolis() {
+	double alpha = 0.5;
+
+	int n = int(frand()*N);
+	double xTrial = x[n] + delta * norm();
+
+	if (weight(xTrial, x[n], alpha) > frand()) {
+		x[n] = xTrial;
+
+	}
+}
+
+void step_montecarlo() {
+	for (int i = 0; i < N; i++) {
+		step_metropolis();
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	initialize();
+
+	return 0;
+}
